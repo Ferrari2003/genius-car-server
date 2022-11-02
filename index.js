3c19b34076a12687c96dbc219b78ce04ec499e7e
Saiful Islam <https://github.com/Ferrari2003>
2022-11-02 19:38:12 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,86 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+
+// middle wares
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.wjha8kd.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const orderCollection = client.db('geniusCar').collection('orders');
+
+async function run(){
+    try{
+        const serviceCollection = client.db('geniusCars').collection('services');
+
+        app.get('/services', async(req, res)=> {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services)
+        })
+
+        app.get('/services/:id', async(req, res)=> {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const services = await serviceCollection.findOne(query);
+            res.send(services)
+        })
+
+
+        // order API
+        app.get('/orders', async(req, res)=> {
+            let query = {};
+            if(req.query.email){
+                query = {
+                    email: req.query.email
+                }
+            }
+            const cursor = orderCollection.find(query);
+            const orders =await cursor.toArray();
+            res.send(orders)
+        })
+        app.post('/orders',async(req, res)=> {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.send(result);
+        });
+
+        app.patch('/orders/:id', async(req, res) => {
+        const id = req.params.id;
+        const status =req.body.status;
+        const query = {_id: ObjectId(id)}
+        const updatedDoc = {
+            $set:{
+                status:status
+            }
+        }
+        const result = await orderCollection.updateOne(query,updatedDoc)
+        res.send(result);
+      })
+
+        app.delete('/orders/:id', async (req,res)=>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id) };
+            const result = await orderCollection.deleteOne(query);
+            res.send(result);
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(error => console.log(error))
+
+app.get('/', (req, res)=> {
+    res.send('server is running')
+});
+
+app.listen(port, () => {
+    console.log(`backend server running${port}`)
+})
