e6aef629b08233282203b10ea2052d09aeb6e6c8
Saiful Islam <https://github.com/Ferrari2003>
2022-11-11 22:27:48 +0600

    last commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -14,13 +15,49 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 const orderCollection = client.db('geniusCar').collection('orders');
 
+function verifyJWT(req, res, next){
+    const authHeader = req.headers.authorization;
+    if(!authHeader){
+        return res.status(401).send({message: 'unauthorized access'})
+    }
+    const token = authHeader.split(' ')[1];
+
+   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(error, decoded){
+    if(error){
+        return res.status(401).send({message:'Forbidden access'})
+    }
+    req.decoded= decoded;
+    next();
+   })
+}
+
 async function run(){
     try{
         const serviceCollection = client.db('geniusCars').collection('services');
 
+        app.post('/jwt', (req, res)=> {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,)
+            res.send({token})
+        })
+
         app.get('/services', async(req, res)=> {
-            const query = {}
-            const cursor = serviceCollection.find(query);
+            const search = req.query.search
+            console.log(search)
+            let query = {}
+            if(search.length){
+                query = {
+                    $text: {
+                        $search : search
+                    }
+                }
+            }
+            // const query = {price: {$gt: 100,$lt: 300}}
+            // const query = {price: {$eq: 200}}
+            // const query = {price: {$lte: 200}}
+            // const query = {price: {$gte: 200}}
+            const order = req.query.order === 'asc' ? 1 : -1;
+            const cursor = serviceCollection.find(query).sort({price: order});
             const services = await cursor.toArray();
             res.send(services)
         })
@@ -34,7 +71,14 @@ async function run(){
 
 
         // order API
-        app.get('/orders', async(req, res)=> {
+        app.get('/orders',verifyJWT, async(req, res)=> {
+
+       const decoded =req.decoded;
+     
+       if(decoded.email !== req.query.email){
+        res.status(403).send({message: 'unauthorized access'})
+       }
+
             let query = {};
             if(req.query.email){
                 query = {
